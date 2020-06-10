e8b0bf4172627ae2896500f43bc42915db1af704
yts1999 <yts1999@yeah.net>
2020-06-10 15:57:17 +0800

    modify cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -14,7 +14,7 @@ func Cors() gin.HandlerFunc {
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
 	if gin.Mode() == gin.ReleaseMode {
 		// 生产环境需要配置跨域域名，否则403
-		config.AllowOrigins = []string{"http://yts1999.top:2000", "http://www.yts1999.top:2000", "http://39.106.48.217:2000"}
+		config.AllowOrigins = []string{"http://yts1999.top:3000", "http://www.yts1999.top:3000", "http://39.106.48.217:3000"}
 	} else {
 		// 测试环境下模糊匹配本地开头的请求
 		config.AllowOriginFunc = func(origin string) bool {
