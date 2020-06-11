e704d7a283b25d351a04dd6202ce89341aabfc6b
yts1999 <yts1999@yeah.net>
2020-06-11 16:22:19 +0800

    bug fix

--- service/room_list_service.go
+++ service/room_list_service.go
@@ -11,10 +11,25 @@ type RoomListService struct {
 
 // List 获取所有房间信息函数
 func (service *RoomListService) List() serializer.Response {
-	var rooms []model.Room
+	rooms := []model.Room{}
 	if err := model.DB.Find(&rooms).Error; err != nil {
 		return serializer.DBErr("获取所有房间信息失败", err)
 	}
+	centerStatusLock.RLock()
+	windSupplyLock.RLock()
+	for i := 0; i < len(rooms); i++ {
+		if rooms[i].WindSupply {
+			var record model.Record
+			if err := model.DB.First(&record, rooms[i].CurrentRecord).Error; err != nil {
+				centerStatusLock.Unlock()
+				return serializer.SystemErr("无法查询当前记录", err)
+			}
+			rooms[i].Energy += record.Energy
+			rooms[i].Bill += record.Bill
+		}
+	}
+	windSupplyLock.RUnlock()
+	centerStatusLock.RUnlock()
 	resp := serializer.BuildRoomsResponse(rooms)
 	resp.Msg = "获取所有房间信息成功"
 	return resp
